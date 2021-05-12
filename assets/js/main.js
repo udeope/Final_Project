bcc253bfab8821f6f9e042c5493ebbb999e42adc
Eduard <edusv10@gmail.com>
2021-05-12 04:52:16 +0200

    Added more html pages

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+var btn = $('#buttonUp');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 100) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '100');
+});
+
+
